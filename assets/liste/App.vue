d69deac7319d3fd06c3e2392f487ada5fb42f655
Marouan <kmarouan5@gmail.com>
2022-01-03 01:32:48 +0100

    api mise a jour

---
+++ assets/liste/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <router-view></router-view>
+
+  </div>
+
+</template>
+
+<script>
+
+import listAnnonce from "./components/listAnnonce";
+import afficheAnnonce from "./components/afficheAnnonce";
+
+export default {
+  name: "App",
+  components:{
+    listAnnonce,
+    afficheAnnonce
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
